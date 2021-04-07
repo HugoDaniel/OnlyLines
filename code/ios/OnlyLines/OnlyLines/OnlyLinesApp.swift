b41178fce122aa3d722b1c2995775b7407f8bbcb
Hugo Gomes <mail@hugodaniel.pt>
2021-04-07 10:20:57 +0100

    First version

---
+++ code/ios/OnlyLines/OnlyLines/OnlyLinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlyLinesApp.swift
+//  OnlyLines
+//
+//  Created by Semba on 07/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct OnlyLinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ code/ios/OnlyLines/OnlyLines/OnlyLinesView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnlyLines
+//
+//  Created by Semba on 07/04/2021.
+//
+
+import SwiftUI
+
+struct OnlyLinesView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnlyLinesView()
+    }
+}
